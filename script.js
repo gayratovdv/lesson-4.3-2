c3679b3601e7c7360be8c37f2ceb9796f2befd35
javohirgvdev <gayratovjavohir90@gmail.com>
2024-03-14 11:22:03 +0500

    I added html and script

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Sizda hozir random son chiqishni boshlaydi")
+
+let kopaytirish = Math.floor(Math.random() * 100)
+
+alert("Random orqali chiqqan son bu: " + (kopaytirish))
+
+alert("Biz random chiqqan sonni 2 ga ko'paytirmoqchimiz va bo'lmoqchimiz")
+
+alert("Random orqali chiqqan son: " + (kopaytirish) + " edi" + "\n" + "biz uni 2 ga ko'paytirib " + (kopaytirish * 2) + " sonini chiqardik" + "\n" + "va shu sonni 2 ga bo'lib " + (kopaytirish / 2) + " bu sonni chiqardik" + "\n" + "va shu sonni 3 ga bo'lgandagi qoldiq bu: " + (kopaytirish % 3))
